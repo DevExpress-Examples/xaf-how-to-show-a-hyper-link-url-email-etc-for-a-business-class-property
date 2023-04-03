379d176ad50c72645511394747f5a077096734af
andrey <andrey.kozhevnikov@devexpress.com>
2023-04-03 17:19:18 +0400

    rem

--- CS/EFCore/HyperLinkEditorEF/HyperLinkEditorEF.Blazor.Server/Editors/HyperLinkPropertyEditor/HyperLinkModel.cs
+++ CS/EFCore/HyperLinkEditorEF/HyperLinkEditorEF.Blazor.Server/Editors/HyperLinkPropertyEditor/HyperLinkModel.cs
@@ -11,15 +11,6 @@ namespace HyperLinkEditor.Blazor.Server.Editors.HyperLinkProperyEditor {
             get => GetPropertyValue<string>();
             set => SetPropertyValue(value);
         }
-        public bool ReadOnly {
-            get => GetPropertyValue<bool>();
-            set => SetPropertyValue(value);
-        }
-        // ...
-        public void SetValueFromUI(string value) {
-            SetPropertyValue(value, notify: false, nameof(Value));
-            ValueChanged?.Invoke(this, EventArgs.Empty);
-        }
         public event EventHandler ValueChanged;
     }
 }
