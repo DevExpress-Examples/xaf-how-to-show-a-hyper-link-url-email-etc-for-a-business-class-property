454d019fda9758d8a0f15531e0af0af17f59591d
andrey <andrey.kozhevnikov@devexpress.com>
2024-11-12 09:16:53 +0400

    show in a list view

--- CS/XPO/HyperLinkEditor/HyperLinkEditor.Blazor.Server/Editors/HyperLinkPropertyEditor/HyperLinkPropertyEditor.cs
+++ CS/XPO/HyperLinkEditor/HyperLinkEditor.Blazor.Server/Editors/HyperLinkPropertyEditor/HyperLinkPropertyEditor.cs
@@ -2,11 +2,22 @@
 using DevExpress.ExpressApp.Blazor.Editors;
 using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
+using Microsoft.AspNetCore.Components;
+using DevExpress.ExpressApp.Blazor;
 
 namespace HyperLinkEditor.Blazor.Server.Editors.HyperLinkProperyEditor {
     [PropertyEditor(typeof(string), false)]
     public class HyperLinkPropertyEditor : BlazorPropertyEditorBase {
         public HyperLinkPropertyEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model) { }
         protected override IComponentAdapter CreateComponentAdapter() => new HyperLinkAdapter(new HyperLinkModel());
+
+        protected override RenderFragment CreateViewComponentCore(object dataContext) {
+            HyperLinkModel componentModel = new HyperLinkModel();
+            componentModel.Value = (string)this.GetPropertyValue(dataContext);
+            componentModel.DisplayValue =  componentModel.Value;
+            componentModel.SetAttribute("readonly", true);
+            var adap=new HyperLinkAdapter(componentModel);
+            return adap.GetComponentContent();
+        }
     }
 }
